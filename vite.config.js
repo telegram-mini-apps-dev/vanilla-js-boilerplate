cc55c6e1be2ba1ec080ef12f372c6939f063aa56
陈永坚 <chenyongjian@xinyu668.com>
2025-01-08 13:52:48 +0800

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,14 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [vue()],
-//   base: '/sexsex/',
-// })
 export default defineConfig({
   plugins: [vue()],
-  server: {
-    host: '0.0.0.0', // Listen on all network interfaces
-    port: 5173, // Default Vite port
-  }
-}) 
+  base: '/sexsex/',
+})
+// export default defineConfig({
+//   plugins: [vue()],
+//   server: {
+//     host: '0.0.0.0', // Listen on all network interfaces
+//     port: 5173, // Default Vite port
+//   }
+// }) 
