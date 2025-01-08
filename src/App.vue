8dcfc5b0370c5fc54e9d85a801dbe3b8fde84db1
陈永坚 <chenyongjian@xinyu668.com>
2025-01-08 16:19:18 +0800

    add

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,18 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div id="app">
+    <MainLayout />
   </div>
-  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script>
+import ImageWaterfall from './pages/ImageWaterfall.vue'
+// import Home from './laa/Home.vue'
+import MainLayout from './layouts/MainLayout.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MainLayout
+  }
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+</script>
