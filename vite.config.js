6cdd23a53230ddfe6b1c6dd0fe69c4bada61970e
陈永坚 <chenyongjian@xinyu668.com>
2025-01-08 11:12:21 +0800

    init github page

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/sexsex/',
 })
